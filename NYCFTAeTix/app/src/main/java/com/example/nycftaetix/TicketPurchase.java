88152db84f4f2d2e6e51e742f0df75a6c8f06ecd
BrianaFigueroa <bf37913n@pace.edu>
2021-04-09 08:20:52 -0400

    Bit of a test to avoid crashes --- Standby

---
+++ NYCFTAeTix/app/src/main/java/com/example/nycftaetix/TicketPurchase.java
@@ -0,0 +1,14 @@
+package com.example.nycftaetix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TicketPurchase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_purchase);
+    }
+}
