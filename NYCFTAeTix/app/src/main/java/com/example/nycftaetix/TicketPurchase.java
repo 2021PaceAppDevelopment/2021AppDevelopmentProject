5d2bb4f53e51836c5450de59c56ef23ccd66f17f
BrianaFigueroa <bf37913n@pace.edu>
2021-04-10 00:52:11 -0400

    QR code can now be created needs button to link to it still and text to change on date and time

--- NYCFTAeTix/app/src/main/java/com/example/nycftaetix/TicketPurchase.java
+++ NYCFTAeTix/app/src/main/java/com/example/nycftaetix/TicketPurchase.java
@@ -2,13 +2,35 @@ package com.example.nycftaetix;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.google.zxing.WriterException;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 
 public class TicketPurchase extends AppCompatActivity {
+    //EditText qrValue; might need later
+    ImageView qrCode;
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_purchase);
+        qrCode = findViewById(R.id.QrCode);
+        String data = "Activated";
+        QRGEncoder qrgEncoder = new QRGEncoder(data,null, QRGContents.Type.TEXT,1000);
+        try {
+            // Getting QR-Code as Bitmap
+            bitmap = qrgEncoder.getBitmap();
+            // Setting Bitmap to ImageView
+            qrCode.setImageBitmap(bitmap);
+        } catch (Exception e) {
+            Log.wtf("MAIN_ACTIVITY", e.toString());
+        }
     }
 }
