bc30b75d48abb0806f81583e8a61380aa7bd406b
Gaelle Gilles <67564107+gia534@users.noreply.github.com>
2021-04-20 20:04:47 -0400

    Small updates

--- NYCFTAeTix/app/src/main/java/com/example/nycftaetix/Tickets.java
+++ NYCFTAeTix/app/src/main/java/com/example/nycftaetix/Tickets.java
@@ -23,7 +23,9 @@ public class Tickets extends AppCompatActivity {
     private Chip oneWayChip;
     private Chip monthlyChip;
     private Chip weeklyChip;
-    private AlertDialog alertDialog;
+    private AlertDialog alertDialogOne;
+    //private ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,26 +43,37 @@ public class Tickets extends AppCompatActivity {
     }
 
     public void onMonthlyTickets(View view) {
-        alertDialog = new MaterialAlertDialogBuilder(Tickets.this)
+        alertDialogOne = new MaterialAlertDialogBuilder(Tickets.this)
                 .setTitle("Purchase Tickets")
                 .setMessage("Monthly tickets cost $127.00. " +
                         "Would you like to proceed with the purchase?")
                 .setNegativeButton("No", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
+                        dialog.dismiss();
                     }
                 })
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                    AlertDialog.Builder alertDialogTwo = new AlertDialog.Builder(Tickets.this);
+
+                                alertDialogTwo.setTitle("Purchase complete")
+                                .setMessage("Monthly tickets last for 30 days from date of activation." +
+                                        "You may activate your monthly tickets whenever you need it.")
+                                .setNeutralButton("Close", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                }).show();
 
                     }
                 }).show();
     }
 
     public void onWeeklyTickets(View view) {
-        alertDialog = new MaterialAlertDialogBuilder(Tickets.this)
+        alertDialogOne = new MaterialAlertDialogBuilder(Tickets.this)
                 .setTitle("Purchase Tickets")
                 .setMessage("Weekly tickets cost $33.00. " +
                         "Would you like to proceed with the purchase?")
@@ -73,26 +86,47 @@ public class Tickets extends AppCompatActivity {
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        AlertDialog.Builder alertDialogTwo = new AlertDialog.Builder(Tickets.this);
+
+                        alertDialogTwo.setTitle("Purchase complete")
+                                .setMessage("Weekly tickets last for 7 days from date of activation." +
+                                        "You may activate your weekly tickets whenever you need it.")
+                                .setNeutralButton("Close", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                }).show();
 
                     }
                 }).show();
     }
 
     public void onOneWayTickets(View view) {
-        alertDialog = new MaterialAlertDialogBuilder(Tickets.this)
+        alertDialogOne = new MaterialAlertDialogBuilder(Tickets.this)
                 .setTitle("Purchase Tickets")
                 .setMessage("One way tickets cost $2.75. " +
                         "Would you like to proceed with the purchase?")
                 .setNegativeButton("No", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
+                        dialog.dismiss();
                     }
                 })
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        AlertDialog.Builder alertDialogTwo = new AlertDialog.Builder(Tickets.this);
 
+                        alertDialogTwo.setTitle("Purchase complete")
+                                .setMessage("One-way tickets can be used for one bus ride." +
+                                        "You may activate your weekly tickets whenever you need it.")
+                                .setNeutralButton("Close", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                }).show();
                     }
                 }).show();
     }
