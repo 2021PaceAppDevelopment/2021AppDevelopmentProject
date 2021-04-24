57f983f10df71fa7c4acb954dce069bb702dcfa0
BrianaFigueroa <bf37913n@pace.edu>
2021-04-24 10:55:14 -0400

    working on profile and Helper page

--- NYCFTAeTix/app/src/main/java/com/example/nycftaetix/Profile.java
+++ NYCFTAeTix/app/src/main/java/com/example/nycftaetix/Profile.java
@@ -8,19 +8,29 @@ import androidx.fragment.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 
 public class Profile extends AppCompatActivity {
     private FragmentManager fragmentManager;
     private Fragment fragment = null;
-
+    private FirebaseAuth firebaseAuth;
+    private TextView textViewemailname;
+    private DatabaseReference databaseReference;
+    private EditText cNumber;
+    private EditText CVN;
+    private
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
     }
 
     public void onTickets(View view) {
